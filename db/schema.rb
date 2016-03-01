9122bc16ad6f4221d8bc8f665dd402ab8731c1b0
Mic <mic.cassano@gmail.com>
2016-03-01 11:48:46 +0000

    Add GET single restaurant route

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160301103841) do
+ActiveRecord::Schema.define(version: 20160301112735) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,8 +20,9 @@ ActiveRecord::Schema.define(version: 20160301103841) do
     t.string   "name"
     t.string   "string"
     t.integer  "rating"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "description"
   end
 
 end
