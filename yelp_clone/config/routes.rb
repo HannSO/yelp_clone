f948c81af42e741f1d8f59d2e638eca541574088
Marion LV <marion.levisage@gmail.com>
2016-03-02 10:04:47 +0000

    OAuth initialisation

--- yelp_clone/config/routes.rb
+++ yelp_clone/config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
 
-  root to: "restaurants#index"
-
   devise_for :users
-  get 'restaurants' => 'restaurants#index'
 
   resources :restaurants do
     resources :reviews
   end
+
+  root to: "restaurants#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
