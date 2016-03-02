ba553f8bdba4c98bd249ab60731f911489492e61
Marion LV <marion.levisage@gmail.com>
2016-03-02 10:51:28 +0000

    Initialised FB login

--- yelp_clone/config/routes.rb
+++ yelp_clone/config/routes.rb
@@ -7,6 +7,12 @@ Rails.application.routes.draw do
   end
 
   root to: "restaurants#index"
+
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
+  devise_scope :user do
+    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
